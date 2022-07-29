6372c1b3dc531fc94ac127f2f65711323a358692
Cooper Saye <coopersaye@gmail.com>
2022-07-28 23:19:53 -0700

    Put up coming soon message.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,17 @@ import styles from '../styles/pages/Index.module.scss';
 export default function Index() {
   return (
     <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>coop.codes</h1>
+        <p>coming soon</p>
+        <a
+          href="https://twitter.com/intent/follow?screen_name=CooperComputer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow @CooperComputer on Twitter for updates
+        </a>
+      </div>
     </div>
   );
 }
