36acef63cefc6690f8f80e162e585f3ca3762a0f
Cooper Saye <coopersaye@gmail.com>
2022-08-01 19:32:01 -0700

    Update favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,10 @@ export default function App(props: AppProps) {
       <Head>
         <title>coop.codes</title>
         <meta name="description" content="A collection of fun web experiments." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
