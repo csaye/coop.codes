4726265fc9d6906cbe934ce1b6f0924a085d8b90
Cooper Saye <coopersaye@gmail.com>
2022-08-01 19:33:26 -0700

    Update index page layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,64 @@
+import { Email, GitHub, Twitter } from '@mui/icons-material';
+import { useState } from 'react';
+import KofiButton from '../components/KofiButton';
+import Snake from '../components/Snake';
 import styles from '../styles/pages/Index.module.scss';
 
 export default function Index() {
+  const [fade, setFade] = useState(false);
+
   return (
     <div className={styles.container}>
-      <div className={styles.center}>
-        <h1>coop.codes</h1>
-        <p>coming soon</p>
-        <a
-          href="https://twitter.com/intent/follow?screen_name=CooperComputer"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Snake fade={fade} setFade={setFade} />
+      <div
+        style={{ opacity: fade ? 0 : 1 }}
+        className={styles.kofiButton}
+      >
+        <KofiButton />
+      </div>
+      <div className={styles.content}>
+        <div
+          className={styles.center}
+          style={fade ? { opacity: 0.5 } : {}}
         >
-          Follow @CooperComputer on Twitter for updates
-        </a>
+          <h1>
+            <span>&gt;{' '}</span>
+            {
+              'coop.codes'.split('').map((char, i) =>
+                <span
+                  style={{ animationDelay: `${(i + 1) * 0.1}s` }}
+                  key={i}
+                >
+                  {char}
+                </span>
+              )
+            }
+          </h1>
+          <p>coming soon</p>
+          <div className={styles.icons}>
+            <a
+              href="https://github.com/csaye"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHub />
+            </a>
+            <a
+              href="mailto:hi@coop.codes"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Email />
+            </a>
+            <a
+              href="https://twitter.com/CooperComputer"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Twitter />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
