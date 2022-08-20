95fcc7b9e4c9bc2262cd72d272edf26b917ade74
Cooper Saye <coopersaye@gmail.com>
2022-08-20 13:35:41 -0700

    Clean up searched word.

--- pages/domain-hacker.tsx
+++ pages/domain-hacker.tsx
@@ -21,6 +21,22 @@ export default function DomainHacker() {
   const questionMark = <span style={{ color: 'goldenrod' }}><b>?</b></span>;
   const dotMark = <span style={{ color: 'black' }}><b> • </b></span>;
 
+  // finds hacks for given word
+  async function findHacks(word: string) {
+    // clean up word
+    word = word.toLowerCase();
+    word = word.replace(/[^a-z]+/g, '');
+    // verify word
+    if (!word) {
+      window.alert('Please enter letter characters.');
+      return;
+    }
+    if (word.length < 2) {
+      window.alert('Please enter at least 2 letters.');
+      return;
+    }
+  }
+
   return (
     <div className={styles.container}>
     </div>
