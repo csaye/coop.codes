96336afcd82170c61265183f00b0e133a5594082
Cooper Saye <coopersaye@gmail.com>
2022-08-20 13:37:41 -0700

    Create whois api endpoint.

---
+++ pages/api/whois.js
@@ -0,0 +1,15 @@
+const whois = require('whois-parsed');
+
+// handles domain whois lookups
+export default async function handler(req, res) {
+  try {
+    // race lookup with 10 second maximum timeout
+    const json = await Promise.race([
+      new Promise(res => setTimeout(res, 10000)),
+      whois.lookup(req.query.domain)
+    ]);
+    res.status(200).json(json);
+  } catch (e) {
+    res.status(200).json(null);
+  }
+}
