9d9eb630f4ea6060c108770c2d3bc03bd8c7dc59
Cooper Saye <coopersaye@gmail.com>
2022-08-20 13:34:51 -0700

    Update hacker page props.

--- pages/domain-hacker.tsx
+++ pages/domain-hacker.tsx
@@ -1,6 +1,26 @@
+import { ReactNode, useState } from 'react';
 import styles from '../styles/pages/DomainHacker.module.scss';
+import { namecheapTlds } from '../util/namecheapTlds';
+import { tlds } from '../util/tlds';
+
+type Hack = {
+  available: 'loading' | undefined | true | false,
+  tld: string,
+  domain: string,
+  node: ReactNode
+};
 
 export default function DomainHacker() {
+  const [word, setWord] = useState('');
+  const [foundHacks, setHacks] = useState<Hack[]>([]);
+
+  // mark nodes
+  const checkMark = <span style={{ color: 'green' }}><b>✓</b></span>;
+  const xMark = <span style={{ color: 'red' }}><b>✘</b></span>;
+  const dotsMark = <span style={{ color: 'gray' }}><b>…</b></span>;
+  const questionMark = <span style={{ color: 'goldenrod' }}><b>?</b></span>;
+  const dotMark = <span style={{ color: 'black' }}><b> • </b></span>;
+
   return (
     <div className={styles.container}>
     </div>
