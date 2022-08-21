8aa2cd99673f53c449eca330b85a9f21c7f6ad14
Cooper Saye <coopersaye@gmail.com>
2022-08-20 23:55:56 -0700

    Update index page content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Email, GitHub, Twitter } from '@mui/icons-material';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useState } from 'react';
+import HomeLink from '../components/HomeLink';
 import KofiButton from '../components/KofiButton';
 import Snake from '../components/Snake';
 import styles from '../styles/pages/Index.module.scss';
@@ -7,13 +9,9 @@ import styles from '../styles/pages/Index.module.scss';
 export default function Index() {
   const [fade, setFade] = useState(false);
 
-  // prevent body overflow
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-  }, []);
-
   return (
     <div className={styles.container}>
+      <HomeLink />
       <Snake fade={fade} setFade={setFade} />
       <div
         style={{ opacity: fade ? 0 : 1 }}
@@ -39,7 +37,11 @@ export default function Index() {
               )
             }
           </h1>
-          <p>coming soon</p>
+          <p>
+            <Link href="/domain-hacks">
+              <a>Domain Hacks</a>
+            </Link>
+          </p>
           <div className={styles.icons}>
             <a
               href="https://github.com/csaye"
