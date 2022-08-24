97258ff8ad034bfd7424e7f8c48ce98325000279
Cooper Saye <coopersaye@gmail.com>
2022-08-23 18:36:22 -0700

    Create TextWidth component.

--- pages/i-acute.tsx
+++ pages/i-acute.tsx
@@ -1,4 +1,22 @@
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { Tooltip, useMediaQuery } from '@mui/material';
+import Link from 'next/link';
+import { useState } from 'react';
+import Header from '../components/Header';
 import styles from '../styles/pages/IAcute.module.scss';
+import { accentedLetters, letters, pathLetters, TextWidth as TextWidthType } from '../util/letterWidths';
+import { shortTlds } from '../util/shortTlds';
+
+function TextWidth(props: TextWidthType) {
+  const { text, width } = props;
+
+  return (
+    <div className="defaultFont">
+      <span>{text}</span>
+      <span style={{ fontSize: '12px' }}> ({width.toFixed(2)} pixels)</span>
+    </div>
+  );
+}
 
 export default function IAcute() {
   return (
