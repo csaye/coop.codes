d748e3d8c5f145a5a3f304019f65fe77f97167df
Cooper Saye <coopersaye@gmail.com>
2022-08-23 18:34:45 -0700

    Define letter widths.

---
+++ util/letterWidths.tsx
@@ -0,0 +1,111 @@
+export type TextWidth = {
+  text: string,
+  width: number
+};
+
+export const letters: TextWidth[] = [
+  {
+    text: "j",
+    width: 3.6328125
+  },
+  {
+    text: "i",
+    width: 3.640625
+  },
+  {
+    text: "l",
+    width: 3.734375
+  },
+  {
+    text: "f",
+    width: 5.4765625
+  },
+  {
+    text: "t",
+    width: 5.5
+  },
+  {
+    text: "r",
+    width: 5.78125
+  },
+  {
+    text: "s",
+    width: 8.0625
+  },
+  {
+    text: "x",
+    width: 8.078125
+  },
+  {
+    text: "z",
+    width: 8.3125
+  },
+  {
+    text: "v",
+    width: 8.359375
+  },
+  {
+    text: "k",
+    width: 8.375
+  },
+  {
+    text: "y",
+    width: 8.375
+  },
+  {
+    text: "a",
+    width: 8.515625
+  },
+  {
+    text: "c",
+    width: 8.640625
+  },
+  {
+    text: "e",
+    width: 8.828125
+  },
+  {
+    text: "n",
+    width: 9.0234375
+  },
+  {
+    text: "u",
+    width: 9.0234375
+  },
+  {
+    text: "h",
+    width: 9.1015625
+  },
+  {
+    text: "o",
+    width: 9.140625
+  },
+  {
+    text: "g",
+    width: 9.4375
+  },
+  {
+    text: "q",
+    width: 9.4375
+  },
+  {
+    text: "p",
+    width: 9.453125
+  },
+  {
+    text: "b",
+    width: 9.515625
+  },
+  {
+    text: "d",
+    width: 9.515625
+  },
+  {
+    text: "w",
+    width: 12.078125
+  },
+  {
+    text: "m",
+    width: 13.609375
+  }
+];
