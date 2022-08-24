656d4cc229cd209175a96f5ebd606f376d1e3cef
Cooper Saye <coopersaye@gmail.com>
2022-08-23 18:37:03 -0700

    Define acute page variables.

--- pages/i-acute.tsx
+++ pages/i-acute.tsx
@@ -19,6 +19,22 @@ function TextWidth(props: TextWidthType) {
 }
 
 export default function IAcute() {
+  const [copyText, setCopyText] = useState('Click to copy');
+
+  // big screen media query
+  const bigScreen = useMediaQuery('(min-width: 700px)');
+
+  // mark nodes
+  const checkMark = <span style={{ color: 'green' }}><b>✓</b></span>;
+  const xMark = <span style={{ color: 'red' }}><b>✘</b></span>;
+
+  // copies í.is to the clipboard
+  async function copyLink(link: string) {
+    await navigator.clipboard.writeText(link);
+    setCopyText('Copied');
+    setTimeout(() => setCopyText('Click to copy'), 2000);
+  }
+
   return (
     <div className={styles.container}>
     </div>
