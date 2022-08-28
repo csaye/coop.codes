c160e61a2ccbbfcdd51c7c18ddf4e136977b3216
Cooper Saye <coopersaye@gmail.com>
2022-08-27 23:16:21 -0400

    Set up five second timeout race.

--- pages/api/whois.js
+++ pages/api/whois.js
@@ -3,14 +3,15 @@ import parseWhois from '../../util/parseWhois';
 
 // handles domain whois lookups
 export default async function handler(req, res) {
-  try {
-    // race lookup with 10 second maximum timeout
-    const json = await Promise.race([
-      new Promise(res => setTimeout(res, 10000)),
-      whois.lookup(req.query.domain)
-    ]);
-    res.status(200).json(json);
-  } catch (e) {
-    res.status(200).json(null);
-  }
+  // get domain from query
+  const { domain } = req.query;
+  // rave timeout with whois lookup
+  const result = await Promise.race([
+    // five second timeout
+    new Promise(resolve => setTimeout(() => {
+      resolve(null);
+    }, 5000)),
+  ]);
+  // resolve with result
+  res.status(200).json(result);
 }
